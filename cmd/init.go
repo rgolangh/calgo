6f490e14d5e6c30d0a3640083fdf87e37412b439
Roy Golan <rgolan@redhat.com>
2022-08-09 11:47:05 +0300

    init: ask for client id and secret, add instructions
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- cmd/init.go
+++ cmd/init.go
@@ -16,38 +16,51 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "os"
 
-	"github.com/spf13/cobra"
-	"golang.org/x/crypto/ssh/terminal"
+    "github.com/spf13/cobra"
+    "golang.org/x/crypto/ssh/terminal"
 )
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+    Use:   "init",
+    Short: "A brief description of your command",
+    Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("init called")
-		token, err := terminal.ReadPassword(int(os.Stdin.Fd()))
-		if err != nil {
-			return err
-		}
+    RunE: func(cmd *cobra.Command, args []string) error {
+        fmt.Printf("" +
+            "Lets setup the google calendar integration:\n" +
+            "1. enable google calendar API for your account - https://console.cloud.google.com/apis/api/calendar-json.googleapis.com \n" +
+            "2. create credentials:\n" +
+            "   - credentials type: user-data,\n" +
+            "   - scopes: 'https://www.googleapis.com/auth/calendar,\n" +
+            "3. Go to 'credentials' https://console.cloud.google.com/apis/credentials/\n" +
+            "   and fill in the details here:\n")
 
-		// save the token
-		fmt.Printf("token is %s\n", token)
+        fmt.Printf("\nCLIENT_ID:")
+        clientId, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+        if err != nil {
+            return err
+        }
+        fmt.Printf("\nCLIENT_SECRET:")
+        clientSecret, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+        if err != nil {
+            return err
+        }
+        // save the token
+        fmt.Printf("\nclient id: %s\nclient secret %s\n", clientId, clientSecret)
 
-		// init google client with it
-		return nil
-	},
+        // init google client with it
+        return nil
+    },
 }
 
 func init() {
-	rootCmd.AddCommand(initCmd)
+    rootCmd.AddCommand(initCmd)
 }
