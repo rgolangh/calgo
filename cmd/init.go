73c576cd090ebd14ecb8f6c47b18d70d17fc5c57
Roy Golan <rgolan@redhat.com>
2022-08-10 10:20:43 +0300

    Basic oauth and redirect works
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- cmd/init.go
+++ cmd/init.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,11 +16,21 @@ limitations under the License.
 package cmd
 
 import (
+    "context"
+    "encoding/json"
     "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
     "os"
+    "time"
+
+    "google.golang.org/api/option"
 
     "github.com/spf13/cobra"
-    "golang.org/x/crypto/ssh/terminal"
+    "golang.org/x/oauth2"
+    "golang.org/x/oauth2/google"
+    "google.golang.org/api/calendar/v3"
 )
 
 // initCmd represents the init command
@@ -34,28 +44,71 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
     RunE: func(cmd *cobra.Command, args []string) error {
-        fmt.Printf("" +
-            "Lets setup the google calendar integration:\n" +
-            "1. enable google calendar API for your account - https://console.cloud.google.com/apis/api/calendar-json.googleapis.com \n" +
-            "2. create credentials:\n" +
-            "   - credentials type: user-data,\n" +
-            "   - scopes: 'https://www.googleapis.com/auth/calendar,\n" +
-            "3. Go to 'credentials' https://console.cloud.google.com/apis/credentials/\n" +
-            "   and fill in the details here:\n")
+        //fmt.Printf("" +
+        //    "Lets setup the google calendar integration:\n" +
+        //    "1. enable google calendar API for your account - https://console.cloud.google.com/apis/api/calendar-json.googleapis.com \n" +
+        //    "2. create credentials:\n" +
+        //    "   - credentials type: user-data,\n" +
+        //    "   - scopes: 'https://www.googleapis.com/auth/calendar,\n" +
+        //    "3. Go to 'credentials' https://console.cloud.google.com/apis/credentials/\n" +
+        //    "   and fill in the details here:\n")
+        //
+        //fmt.Printf("\nCLIENT_ID:")
+        //clientId, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+        //if err != nil {
+        //    return err
+        //}
+        //fmt.Printf("\nCLIENT_SECRET:")
+        //clientSecret, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+        //if err != nil {
+        //    return err
+        //}
+        //// save the token
+        //fmt.Printf("\nclient id: %s\nclient secret %s\n", clientId, clientSecret)
+        code := make(chan string)
+        go func() {
+            http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+                code <- request.URL.Query().Get("code")
+            })
+            http.ListenAndServe("localhost:8111", nil)
+        }()
 
-        fmt.Printf("\nCLIENT_ID:")
-        clientId, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+        ctx := context.Background()
+        b, err := ioutil.ReadFile("credentials.json")
         if err != nil {
-            return err
+            log.Fatalf("Unable to read client secret file: %v", err)
         }
-        fmt.Printf("\nCLIENT_SECRET:")
-        clientSecret, err := terminal.ReadPassword(int(os.Stdin.Fd()))
-        if err != nil {
-            return err
-        }
-        // save the token
-        fmt.Printf("\nclient id: %s\nclient secret %s\n", clientId, clientSecret)
 
+        // If modifying these scopes, delete your previously saved token.json.
+        config, err := google.ConfigFromJSON(b, calendar.CalendarScope)
+        if err != nil {
+            log.Fatalf("Unable to parse client secret file to config: %v", err)
+        }
+        client := getClient(config, code)
+
+        srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
+        if err != nil {
+            log.Fatalf("Unable to retrieve Calendar client: %v", err)
+        }
+
+        t := time.Now().Format(time.RFC3339)
+        events, err := srv.Events.List("primary").ShowDeleted(false).
+            SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
+        if err != nil {
+            log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
+        }
+        fmt.Println("Upcoming events:")
+        if len(events.Items) == 0 {
+            fmt.Println("No upcoming events found.")
+        } else {
+            for _, item := range events.Items {
+                date := item.Start.DateTime
+                if date == "" {
+                    date = item.Start.Date
+                }
+                fmt.Printf("%v (%v)\n", item.Summary, date)
+            }
+        }
         // init google client with it
         return nil
     },
@@ -63,4 +116,59 @@ to quickly create a Cobra application.`,
 
 func init() {
     rootCmd.AddCommand(initCmd)
+}
+
+func getTokenFromWeb(config *oauth2.Config, code <-chan string) *oauth2.Token {
+    authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+    fmt.Printf("Go to the following link in your browser then type the "+
+        "authorization code: \n%v\n", authURL)
+
+    var authCode string
+    select {
+    case authCode = <-code:
+        fmt.Printf("got the auth-code %s\n", authCode)
+    }
+
+    tok, err := config.Exchange(context.TODO(), authCode)
+    if err != nil {
+        log.Fatalf("Unable to retrieve token from web: %v", err)
+    }
+    return tok
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config, code <-chan string) *http.Client {
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    tokFile := "token.json"
+    tok, err := tokenFromFile(tokFile)
+    if err != nil {
+        tok = getTokenFromWeb(config, code)
+        saveToken(tokFile, tok)
+    }
+    return config.Client(context.Background(), tok)
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+    f, err := os.Open(file)
+    if err != nil {
+        return nil, err
+    }
+    defer f.Close()
+    tok := &oauth2.Token{}
+    err = json.NewDecoder(f).Decode(tok)
+    return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+    fmt.Printf("Saving credential file to: %s\n", path)
+    f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+    if err != nil {
+        log.Fatalf("Unable to cache oauth token: %v", err)
+    }
+    defer f.Close()
+    json.NewEncoder(f).Encode(token)
 }
--- go.mod
+++ go.mod
@@ -6,10 +6,18 @@ require (
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/viper v1.12.0
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
+	golang.org/x/oauth2 v0.0.0-20220808172628-8227340efae7
+	google.golang.org/api v0.91.0
 )
 
 require (
+	cloud.google.com/go/compute v1.7.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
@@ -21,9 +29,15 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
-	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/sys v0.0.0-20220624220833-87e55d714810 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220624142145-8cd45d7dbd1f // indirect
+	google.golang.org/grpc v1.47.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0 // indirect
