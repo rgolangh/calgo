9bb9621eacaf7e1c5919805905825406db05c862
Roy Golan <rgolan@redhat.com>
2022-08-21 20:34:07 +0300

    Add list sub-command
    Signed-off-by: Roy Golan <royg21@gmail.com>

--- cmd/init.go
+++ cmd/init.go
@@ -16,27 +16,14 @@ limitations under the License.
 package cmd
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-    "time"
-
-    "google.golang.org/api/option"
-
+    "github.com/rgolangh/calgo/internal/google_calendar"
     "github.com/spf13/cobra"
-    "golang.org/x/oauth2"
-    "golang.org/x/oauth2/google"
-    "google.golang.org/api/calendar/v3"
 )
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
     Use:   "init",
-    Short: "A brief description of your command",
+    Short: "Initialize the tool with your google credentials",
     Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -44,131 +31,11 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
     RunE: func(cmd *cobra.Command, args []string) error {
-        //fmt.Printf("" +
-        //    "Lets setup the google calendar integration:\n" +
-        //    "1. enable google calendar API for your account - https://console.cloud.google.com/apis/api/calendar-json.googleapis.com \n" +
-        //    "2. create credentials:\n" +
-        //    "   - credentials type: user-data,\n" +
-        //    "   - scopes: 'https://www.googleapis.com/auth/calendar,\n" +
-        //    "3. Go to 'credentials' https://console.cloud.google.com/apis/credentials/\n" +
-        //    "   and fill in the details here:\n")
-        //
-        //fmt.Printf("\nCLIENT_ID:")
-        //clientId, err := terminal.ReadPassword(int(os.Stdin.Fd()))
-        //if err != nil {
-        //    return err
-        //}
-        //fmt.Printf("\nCLIENT_SECRET:")
-        //clientSecret, err := terminal.ReadPassword(int(os.Stdin.Fd()))
-        //if err != nil {
-        //    return err
-        //}
-        //// save the token
-        //fmt.Printf("\nclient id: %s\nclient secret %s\n", clientId, clientSecret)
-        code := make(chan string)
-        go func() {
-            http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-                code <- request.URL.Query().Get("code")
-            })
-            http.ListenAndServe("localhost:8111", nil)
-        }()
-
-        ctx := context.Background()
-        b, err := ioutil.ReadFile("credentials.json")
-        if err != nil {
-            log.Fatalf("Unable to read client secret file: %v", err)
-        }
-
-        // If modifying these scopes, delete your previously saved token.json.
-        config, err := google.ConfigFromJSON(b, calendar.CalendarScope)
-        if err != nil {
-            log.Fatalf("Unable to parse client secret file to config: %v", err)
-        }
-        client := getClient(config, code)
-
-        srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
-        if err != nil {
-            log.Fatalf("Unable to retrieve Calendar client: %v", err)
-        }
-
-        t := time.Now().Format(time.RFC3339)
-        events, err := srv.Events.List("primary").ShowDeleted(false).
-            SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
-        if err != nil {
-            log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
-        }
-        fmt.Println("Upcoming events:")
-        if len(events.Items) == 0 {
-            fmt.Println("No upcoming events found.")
-        } else {
-            for _, item := range events.Items {
-                date := item.Start.DateTime
-                if date == "" {
-                    date = item.Start.Date
-                }
-                fmt.Printf("%v (%v)\n", item.Summary, date)
-            }
-        }
-        // init google client with it
+        google_calendar.Service()
         return nil
     },
 }
 
 func init() {
     rootCmd.AddCommand(initCmd)
-}
-
-func getTokenFromWeb(config *oauth2.Config, code <-chan string) *oauth2.Token {
-    authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-    fmt.Printf("Go to the following link in your browser then type the "+
-        "authorization code: \n%v\n", authURL)
-
-    var authCode string
-    select {
-    case authCode = <-code:
-        fmt.Printf("got the auth-code %s\n", authCode)
-    }
-
-    tok, err := config.Exchange(context.TODO(), authCode)
-    if err != nil {
-        log.Fatalf("Unable to retrieve token from web: %v", err)
-    }
-    return tok
-}
-
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config, code <-chan string) *http.Client {
-    // The file token.json stores the user's access and refresh tokens, and is
-    // created automatically when the authorization flow completes for the first
-    // time.
-    tokFile := "token.json"
-    tok, err := tokenFromFile(tokFile)
-    if err != nil {
-        tok = getTokenFromWeb(config, code)
-        saveToken(tokFile, tok)
-    }
-    return config.Client(context.Background(), tok)
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-    f, err := os.Open(file)
-    if err != nil {
-        return nil, err
-    }
-    defer f.Close()
-    tok := &oauth2.Token{}
-    err = json.NewDecoder(f).Decode(tok)
-    return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-    fmt.Printf("Saving credential file to: %s\n", path)
-    f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-    if err != nil {
-        log.Fatalf("Unable to cache oauth token: %v", err)
-    }
-    defer f.Close()
-    json.NewEncoder(f).Encode(token)
 }
---
+++ cmd/list.go
@@ -0,0 +1,151 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+    "fmt"
+    "github.com/rgolangh/calgo/internal/google_calendar"
+    "google.golang.org/api/calendar/v3"
+    "log"
+    "regexp"
+    "strconv"
+    "strings"
+    "time"
+
+    "github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var listCmd = &cobra.Command{
+    Use:   "list",
+    Short: "List events",
+    Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+    Args: func(cmd *cobra.Command, args []string) error {
+        if len(args) == 0 {
+            return nil
+        }
+
+        // first arg is either a day expression, or a range if it has a hyphen
+        compile, err := regexp.Compile("^\\d+(-\\d+)?$")
+        if err != nil {
+            return err
+        }
+        if !compile.MatchString(args[0]) {
+            return fmt.Errorf("first argument is not a day or range expression")
+        }
+        return nil
+    },
+    RunE: func(cmd *cobra.Command, args []string) error {
+        tmin, tmax, err := getMinMaxStartTimes(args[0])
+        srv := google_calendar.Service()
+
+        events, err := srv.Events.List("primary").ShowDeleted(false).
+            SingleEvents(true).TimeMin(tmin.Format(time.RFC3339)).TimeMax(tmax.Format(time.RFC3339)).MaxResults(10).OrderBy("startTime").Do()
+        if err != nil {
+            log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
+        }
+
+        fmt.Println("Upcoming events:")
+        if len(events.Items) == 0 {
+            fmt.Println("No upcoming events found.")
+        } else {
+            for _, item := range events.Items {
+                date := item.Start.DateTime
+                if date == "" {
+                    date = item.Start.Date
+                }
+                printEvent(item)
+            }
+        }
+
+        return nil
+    },
+}
+
+// getMinMaxTimes get a day, or range expression in the form of
+// n or n-n where n is the day and return 2 datetime representing
+// the min start time and max start time of meeting to search for
+func getMinMaxStartTimes(s string) (time.Time, time.Time, error) {
+    var tmin, tmax time.Time
+    //tmin := time.Now().Format(time.RFC3339)
+    //tmax := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 0, now.Location()).Format(time.RFC3339)
+
+    compile, err := regexp.Compile("^\\d+(-\\d+)?$")
+    if err != nil {
+        return tmin, tmax, err
+    }
+    if !compile.MatchString(s) {
+        return tmin, tmax, fmt.Errorf("first argument is not a day or range expression")
+    }
+
+    isRange := strings.Contains(s, "-")
+    if !isRange {
+        tmax, err = timeFromExpression(s)
+        if err != nil {
+            return tmin, tmax, err
+        }
+    }
+
+    return tmin, tmax, nil
+}
+
+func timeFromExpression(s string) (time.Time, error) {
+
+    t := time.Now()
+    if strings.HasPrefix(s, "+") || strings.HasPrefix(s, "-") {
+        atoi, err := strconv.Atoi(s)
+        if err != nil {
+            return t, err
+        }
+        return t.AddDate(0, 0, atoi), nil
+    }
+
+    // currently supporting extracting dates for this week only
+    switch s {
+    case "1", "s":
+        t.AddDate(0, 0, int(time.Sunday-t.Weekday()))
+    case "2", "m":
+        t.AddDate(0, 0, int(time.Monday-t.Weekday()))
+    case "3", "t":
+        t.AddDate(0, 0, int(time.Tuesday-t.Weekday()))
+    case "4", "w":
+        t.AddDate(0, 0, int(time.Wednesday-t.Weekday()))
+    case "5", "th":
+        t.AddDate(0, 0, int(time.Thursday-t.Weekday()))
+    case "6", "f":
+        t.AddDate(0, 0, int(time.Friday-t.Weekday()))
+    case "7", "sa":
+        t.AddDate(0, 0, int(time.Saturday-t.Weekday()))
+    default:
+        return t, fmt.Errorf("non supported expression %s", s)
+    }
+
+    return t, nil
+}
+
+func printEvent(e *calendar.Event) {
+
+    fmt.Printf("%v - %v\n", e.Start.DateTime, e.Summary)
+}
+
+func init() {
+    rootCmd.AddCommand(listCmd)
+}
